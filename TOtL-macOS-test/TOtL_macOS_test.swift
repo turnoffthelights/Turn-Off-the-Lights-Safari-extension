fd4881104b5b2dd46f66967169b857e4e2d45607
Stefan Van Damme <20127018+stefanvd@users.noreply.github.com>
2022-05-28 13:37:03 +0200

    Update

--- TOtL-macOS-test/TOtL_macOS_test.swift
+++ TOtL-macOS-test/TOtL_macOS_test.swift
@@ -2,7 +2,7 @@
 //  TOtL_macOS_test.swift
 //  TOtL-macOS-test
 //
-//  Created by Stefan Van Damme on 30/06/2021.
+//  Created by Stefan Van Damme on 28/05/2022.
 //
 
 import XCTest
@@ -20,6 +20,9 @@ class TOtL_macOS_test: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
 
     func testPerformanceExample() throws {
