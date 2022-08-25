743d14be77d5085717be997f09193267f8fb5981
Stefan Van Damme <20127018+stefanvd@users.noreply.github.com>
2022-08-25 19:33:28 +0200

    Code improvements

--- iOS (App)/WelcomeGuideViewController.swift
+++ iOS (App)/WelcomeGuideViewController.swift
@@ -52,8 +52,8 @@ class WelcomeGuideViewController: UIViewController {
         lblaction1?.text = lblaction1?.text?.uppercased()
     }
 
-    @IBAction func opennightowlprofile(_ sender: Any) {
-        let youtubeId = "vubVpLm8ldk"
+    func openyoutubeapporweb(text:String){
+        let youtubeId = text
         if let youtubeURL = URL(string: "youtube://\(youtubeId)"),
             UIApplication.shared.canOpenURL(youtubeURL) {
             // redirect to app
@@ -73,27 +73,13 @@ class WelcomeGuideViewController: UIViewController {
             }
         }
     }
+    
+    @IBAction func opennightowlprofile(_ sender: Any) {
+        openyoutubeapporweb(text: "vubVpLm8ldk")
+    }
 
     @IBAction func openeyeprofile(_ sender: Any) {
-        let youtubeId = "3TNYUG9O-u8"
-        if let youtubeURL = URL(string: "youtube://\(youtubeId)"),
-            UIApplication.shared.canOpenURL(youtubeURL) {
-            // redirect to app
-            UIApplication.shared.open(youtubeURL, options: [:], completionHandler: nil)
-        } else if URL(string: "https://www.youtube.com/watch?v=\(youtubeId)") != nil {
-            // redirect through safari
-            //UIApplication.shared.open(youtubeURL, options: [:], completionHandler: nil)
-            
-            let thisurlpost = "https://www.youtube.com/watch?v=\(youtubeId)"
-            let config = SFSafariViewController.Configuration()
-            config.barCollapsingEnabled = true
-            config.entersReaderIfAvailable = false
-
-            if let url = URL(string: thisurlpost) {
-                let vc = SFSafariViewController(url: url, configuration: config)
-                present(vc, animated: true)
-            }
-        }
+        openyoutubeapporweb(text: "3TNYUG9O-u8")
     }
     
     let thisgithub = "https://github.com/turnoffthelights/Turn-Off-the-Lights-Safari-extension"
